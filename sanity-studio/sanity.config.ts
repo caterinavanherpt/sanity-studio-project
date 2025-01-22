b2107680addd8fd7f8435ab59863201778436b59
Caterina Del Balso <caterina.vanherpt@gmail.com>
2025-01-22 11:30:53 -0500

    added media browser and AI Assist

--- sanity-studio/sanity.config.ts
+++ sanity-studio/sanity.config.ts
@@ -3,6 +3,8 @@ import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 import {markdownSchema} from 'sanity-plugin-markdown'
+import {media} from 'sanity-plugin-media'
+import {assist} from '@sanity/assist'
 
 export default defineConfig({
   name: 'default',
@@ -11,7 +13,17 @@ export default defineConfig({
   projectId: '2pterscv',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool(), markdownSchema()],
+  plugins: [
+    structureTool(), 
+    visionTool(), 
+    markdownSchema(),
+    media({
+      creditLine: {
+        enabled: true,
+      }
+    }),
+    assist(),
+  ],
 
   schema: {
     types: schemaTypes,
